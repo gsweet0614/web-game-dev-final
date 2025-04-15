110dd506557cf85f169527b57371f5e503bb3894
gsweet0614 <gagesweet07@gmail.com>
2025-04-15 10:19:03 -0400

    Initial Code Dump

---
+++ button.py
@@ -0,0 +1,36 @@
+import pygame as pg
+
+class Button():
+  def __init__(self, x, y, image, single_click):
+    self.image = image
+    self.rect = self.image.get_rect()
+    self.rect.topleft = (x, y)
+    self.clicked = False
+    self.single_click = single_click
+
+  def draw(self, surface, hover_image=None):
+    action = False
+    #get mouse position
+    pos = pg.mouse.get_pos()
+
+    #check mouseover and clicked conditions
+    if self.rect.collidepoint(pos):
+      if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
+        action = True
+        #if button is a single click type, then set clicked to True
+        if self.single_click:
+          self.clicked = True
+
+    if pg.mouse.get_pressed()[0] == 0:
+      self.clicked = False
+
+    #Hover the button if mouse is over the button
+    if hover_image and self.rect.collidepoint(pos):
+        surface.blit(hover_image, self.rect)
+    else:
+        surface.blit(self.image, self.rect)
+
+    #draw button on screen
+    surface.blit(self.image, self.rect)
+
+    return action

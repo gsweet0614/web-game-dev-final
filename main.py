cd2a63dcc07dd46bb004dc290d34d384c559ae8d
Andrew Stewart <93290561+Andrew-Stewart1@users.noreply.github.com>
2025-04-22 21:31:57 -0400

    Fixed button overlay on coins, made the game easier

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ map_image = pg.image.load('level.png').convert_alpha()
 #turret spritesheets
 turret_spritesheets = []
 for x in range(1, c.TURRET_LEVELS + 1):
-  turret_sheet = pg.image.load(f'assets/turret_{x}.png').convert_alpha()
+  turret_sheet = pg.image.load(f'assets/turrets_{x}.png').convert_alpha()
   turret_spritesheets.append(turret_sheet)
 #individual turret image for mouse cursor
-cursor_turret = pg.image.load('assets/turret1.png').convert_alpha()
+cursor_turret = pg.image.load('assets/turretplacer.png').convert_alpha()
 #enemies
 enemy_images = {
   "weak": pg.image.load('assets/enemy_1.png').convert_alpha(),

0e07e77fe3d1f0e06ae4cb5b0bad060785d603d9
Andrew Stewart <93290561+Andrew-Stewart1@users.noreply.github.com>
2025-04-22 21:33:58 -0400

    Delete gameboy_outdoor_assets_v1.2.tsx

--- gameboy_outdoor_assets_v1.2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="gameboy_outdoor_assets_v1.2" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="../../gb_assets/background_tilesets/gameboy_outdoor_assets_v1.2.png" width="240" height="224"/>
-</tileset>
